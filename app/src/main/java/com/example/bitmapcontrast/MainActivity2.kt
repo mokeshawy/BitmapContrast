92e45b31d2ae130babf0404b809cb52a261e0dd2
mohamed <mokeshawy@gmail.com>
2022-06-09 10:33:19 +0200

    implement cameraX

--- app/src/main/java/com/example/bitmapcontrast/MainActivity2.kt
+++ app/src/main/java/com/example/bitmapcontrast/MainActivity2.kt
@@ -1,23 +1,136 @@
 package com.example.bitmapcontrast
 
+import android.content.pm.PackageManager
 import android.graphics.*
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.core.Camera
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import com.bumptech.glide.Glide
 import com.example.bitmapcontrast.databinding.ActivityMain2Binding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.math.pow
 
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var binding: ActivityMain2Binding
     lateinit var bitmap: Bitmap
+
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var camera: Camera? = null
+    private lateinit var outputDirectory: File
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
 
-        bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_solarus_logo)
-        Glide.with(this).load(handleBitmapCorp(bitmap, 40)).centerInside().into(binding.bitmapIv)
+
+//        bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_toyota)
+//        Glide.with(this).load(handleBitmapCorp(bitmap, 20)).centerInside().into(binding.bitmapIv)
+
+        setOnTakePhotoClicked()
+        checkPermission()
+        outputDirectory = getOutPutDirectory()
+
+
+    }
+
+    private fun setOnTakePhotoClicked() {
+        binding.takePhotoBtn.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun checkPermission() {
+        if (allPermissionGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUEST_PERMISSION, REQUEST_CODE_PERMISSION
+            )
+        }
+    }
+
+    private fun getOutPutDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            preview = Preview.Builder().build()
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+            try {
+                cameraProvider.unbindAll()
+                camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+                preview?.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+            } catch (e: Exception) {
+
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg"
+        )
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOption,
+            ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val saveImage = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $saveImage"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                    bitmap = BitmapFactory.decodeFile(photoFile.toString())
+                    binding.bitmapIv.setImageBitmap(handleBitmapCorp(bitmap, 100))
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.d(TAG, "Photo capture failed: ${exception.message}", exception)
+                }
+
+            }
+        )
+    }
+
+    private fun allPermissionGranted() = REQUEST_PERMISSION.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 9) {
+            if (allPermissionGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Permission not granted by the user", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 
     private fun handleBitmapCorp(bitmap: Bitmap, contrastValue: Int): Bitmap {
@@ -74,4 +187,11 @@ class MainActivity2 : AppCompatActivity() {
         }
     }
 
+    companion object {
+        private const val TAG = "CameraXExample"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSION = 10
+        private val REQUEST_PERMISSION = arrayOf(android.Manifest.permission.CAMERA)
+
+    }
 }
